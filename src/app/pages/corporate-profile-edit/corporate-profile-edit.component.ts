b036f5abb44d0d896e44e7f9dcc4aeb5e9641b6e
johnestes4 <johnestes4@gmail.com>
2017-06-30 13:42:34 -0400

    changed update company to redirect back to the corporate profile

--- src/app/pages/corporate-profile-edit/corporate-profile-edit.component.ts
+++ src/app/pages/corporate-profile-edit/corporate-profile-edit.component.ts
@@ -77,6 +77,8 @@ export class CorporateProfileEditComponent implements OnInit {
     // Mongo cannot update a model if _id field is present in the data provided for the update, so we delete it
     delete model['_id'];
     this.companyService.updateCompany(this.route.snapshot.params['id'], model).toPromise().then(result => console.log(result));
+    window.scrollTo(0, 0);
+    this.router.navigate(['corporate-profile', this.route.snapshot.params['id']]);
   }
 
 }
