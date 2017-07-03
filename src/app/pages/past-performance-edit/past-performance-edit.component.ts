8bc94ef017385fad5033c5842cc9ceb026663e9a
Marc Fournier <Fournier@kaihonua.com>
2017-07-03 13:01:56 -0400

    Data is coming through to the past-performance-edit page. Only one field (title) is connected to the model. Needs all input fields connected and functionality for editing.

--- src/app/pages/past-performance-edit/past-performance-edit.component.ts
+++ src/app/pages/past-performance-edit/past-performance-edit.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PastperformanceService } from '../../services/pastperformance.service';
+import { Location } from '@angular/common'
+
+import { PastPerformance } from '../../classes/past-performance'
+
 
 @Component({
   selector: 'app-past-performance-edit',
+  providers: [PastperformanceService],
   templateUrl: './past-performance-edit.component.html',
   styleUrls: ['./past-performance-edit.component.css']
 })
 export class PastPerformanceEditComponent implements OnInit {
+
+  currentPastPerformance: PastPerformance = new PastPerformance()
 
   agencyType: string[] = ['Pro', 'Amature'];
   officeType: string[] = ['Pro', 'Amature'];
@@ -17,13 +26,24 @@ export class PastPerformanceEditComponent implements OnInit {
   writeWidth: number = 800;
   rate: number = 0
 
-  constructor() { }
+  constructor(
+    private pastPerformanceService: PastperformanceService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public location: Location
+  ) {
+    if ( this.router.url !== 'past-performance-create' ) {
+      this.pastPerformanceService.getPastPerformancebyID(this.route.snapshot.params['id']).toPromise().then(res => this.currentPastPerformance = res[0] );
+      //this.pastPerformanceService.dumbMethod()
+      //console.log(this.pastPerformanceService)
+    }
+  }
 
   ngOnInit() {
   }
 
   uploadImage() {
-    
+
   }
 
 }
