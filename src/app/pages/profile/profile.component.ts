bb695c8d52cb783087b40f26fc2f79fc8673a10f
Marc Fournier <Fournier@kaihonua.com>
2017-08-17 18:50:37 -0400

    No Edit button for profiles if not the logged in user

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,13 +5,14 @@ import { Location } from '@angular/common';
 
 import { User } from '../../classes/user'
 import { UserService } from '../../services/user.service'
+import { AuthService } from "../../services/auth.service"
 
 declare var $: any;
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  providers: [UserService],
+  providers: [UserService, AuthService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
@@ -26,17 +27,19 @@ export class ProfileComponent implements OnInit {
     dates: []
   }
   agencyExperience: any[] = []
+  isActiveProfile: boolean = false
 
 
   constructor(
     private userService: UserService,
     private route: ActivatedRoute,
     private router: Router,
-    public location: Location
+    public location: Location,
+    private auth: AuthService
   ) {
 
     // this.currentUser = this.userService.getTempUser();
-
+    this.auth.isLoggedIn().then(() => this.auth.getLoggedInUser() == this.route.snapshot.params['id']? this.isActiveProfile = true: this.isActiveProfile = false).catch((reason) => "User Login Check failed")
 
     this.userService.getUserbyID(this.route.snapshot.params['id']).toPromise().then((result) => {
       this.currentUser = result;
