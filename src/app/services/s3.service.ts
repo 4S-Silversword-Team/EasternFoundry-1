974a04739d3f61e9adbdfc535f532a8e29c189dc
Marc Fournier <Fournier@kaihonua.com>
2017-08-28 15:41:34 -0400

    Can upload an image to s3 from the company edit page

---
+++ src/app/services/s3.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptionsArgs, Headers } from '@angular/http'
+import { environment } from '../../environments/environment'
+
+import { AuthHttp } from '../classes/auth-http'
+
+import { Observable } from 'rxjs/Observable'
+
+@Injectable()
+export class s3Service {
+
+  authHttp: AuthHttp
+
+  constructor(private http: Http) {
+    this.authHttp = new AuthHttp(http)
+  }
+
+  postPhoto(request: any){
+    console.log("Post Photo Service initiated");
+    var response = this.authHttp.postNoAuthNoJson("http://s3.amazonaws.com/" + environment.bucketName, request)
+      .map(response => response);
+    return response;
+  }
+
+}
